d7444e20cc686cfbbd0371eca28fbbf94f7302aa
BENOITAN <62307212+BENOITAN@users.noreply.github.com>
2020-03-26 16:52:33 +0100

    exercice 4

--- td1/exercie4/exer4.php
+++ td1/exercie4/exer4.php
@@ -1,14 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>ex4</title>
+    <title>exercie 4</title>
 </head>
 <body>
     <a href="?nblignes=2&nbColonnes=2">2x2</a>
     <a href="?nblignes=5&nbColonnes=5">5x5</a>
     <a href="?nblignes=10&nbColonnes=8">10x8</a>
-
-
     <?php
     echo "<table border='1'>
     <tbody>";
@@ -23,11 +21,8 @@
         } 
         echo "</tr>"; 
     } 
-
     echo "</tbody>
     </table>";
-
     ?>
-
 </body>
 </html>
