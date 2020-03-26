d151228cab1f881db926ba882a564d74408e2c4f
BENOITAN <62307212+BENOITAN@users.noreply.github.com>
2020-03-26 15:04:00 +0100

    Add files via upload

--- td1/ex4.php
+++ td1/ex4.php
@@ -1,7 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <title>Hello</title>
 </head>
 <body>
+    if($i<5){
+    // faire quelque chose si $i est strictement inférieur à 5
+}else{
+    // $i est supérieur ou égal à 5
+}
+              for($i=0;$i<$max;$i++){
+    // faire quelque chose avec $i
+}
+<table border='1'>
+    <tbody>
+        <tr><strong>
+            <td>0 - 0</td>
+            <td>0 - 1</td>
+    
+        </strong></tr>
+    </tbody>
+</table>
 </body>
-</html>
+</html> 
