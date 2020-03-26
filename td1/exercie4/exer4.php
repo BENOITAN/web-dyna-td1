844c5987922f4cb412707b9ecbb9bba52af6f2b0
BENOITAN <62307212+BENOITAN@users.noreply.github.com>
2020-03-26 16:17:16 +0100

    suite

--- td1/exercie4/exer4.php
+++ td1/exercie4/exer4.php
@@ -1,36 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>ex4</title>
+<meta charset="UTF-8">
+<title>Hello</title>
 </head>
 <body>
-	<a href="?nblignes=2&nbColonnes=2">2x2</a>
-	<a href="?nblignes=5&nbColonnes=5">5x5</a>
-	<a href="?nblignes=10&nbColonnes=8">10x8</a>
-	<hr>
-	<hr>
-	<hr>
-
-	<?php
-	echo "<p>{$_GET["nbColonnes"]}-{$_GET["nblignes"]}</p>";
-	echo "<table border='1'>
-	<tbody>
-	<tr>"
-	for($i=0;$i<$max;$i++){
-		
-	}
-
-	<td>1-1</td>
-	<td>1-2</td>
-	</tr>
-	<tr>
-	<td>2-1</td>
-	<td>2-2</td>
-	</tr>
-	</tbody>
-	</table>
-	"
-	?>
-		
+<?php
+include "header.php"
+?>
+<table border='1'>
+    <tbody>
+        <?php
+        for($i=1; $i<$_GET["nbligne"]; $i++)
+        {
+            echo"<tr>";
+                for ($j=1; $j<$_GET["nbcolonne"]; $j++)
+                {
+                    echo "<type>";
+                    echo "$i-";
+                    echo "$j";
+                    echo "</td>";
+                }
+            echo "</tr>";
+        }
+        ?>
+    </tbody>   
+</table>  
+<?php
+include "footer.php";
+?>
 </body>
 </html>
