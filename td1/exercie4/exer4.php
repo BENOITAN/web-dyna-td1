ed91501d13eda41c42a6361880477eeb244012df
BENOITAN <62307212+BENOITAN@users.noreply.github.com>
2020-03-26 16:48:16 +0100

    exercice 4

--- td1/exercie4/exer4.php
+++ td1/exercie4/exer4.php
@@ -1,33 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Hello</title>
+    <title>ex4</title>
 </head>
 <body>
-<?php
-include "header.php"
-?>
-<table border='1'>
-    <tbody>
-        <?php
-        for($i=1; $i<$_GET["nbligne"]; $i++)
-        {
-            echo"<tr>";
-                for ($j=1; $j<$_GET["nbcolonne"]; $j++)
-                {
-                    echo "<type>";
-                    echo "$i-";
-                    echo "$j";
-                    echo "</td>";
-                }
-            echo "</tr>";
+    <a href="?nblignes=2&nbColonnes=2">2x2</a>
+    <a href="?nblignes=5&nbColonnes=5">5x5</a>
+    <a href="?nblignes=10&nbColonnes=8">10x8</a>
+
+
+    <?php
+    echo "<table border='1'>
+    <tbody>";
+    for($l=0;$l<=$_GET["nblignes"];$l++){
+
+        if (($l % 2) == 0){
+            $gras = "bold";
         }
-        ?>
-    </tbody>   
-</table>  
-<?php
-include "footer.php";
-?>
+        echo "<tr>"; 
+        for($c=0;$c<=$_GET["nbColonnes"];$c++){ 
+            echo "<td align='center'><p style='font-weight: bold;'>$l-$c</p></td>"; 
+        } 
+        echo "</tr>"; 
+    } 
+
+    echo "</tbody>
+    </table>";
+
+    ?>
+
 </body>
 </html>
