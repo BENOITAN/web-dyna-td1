e1c718b75c537a2208577c79bceda9fb060930e6
BENOITAN <62307212+BENOITAN@users.noreply.github.com>
2020-04-01 09:44:32 +0200

    ex44

--- td1/exercie4/exer4.php
+++ td1/exercie4/exer4.php
@@ -1,28 +1,38 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>exercice 4</title>
+	<title>exercice 4</title>
 </head>
 <body>
-    <a href="?nblignes=2&Colonnes=2">2x2</a>
-    <a href="?nblignes=5&Colonnes=5">5x5</a>
-    <a href="?nblignes=10&Colonnes=8">10x8</a>
-    <?php
-    echo "<table border='1'>
-    <tbody>";
-    for($l=0;$l<=$_GET["nblignes"];$l++){
+	<a href="?nblignes=2&nbColonnes=2">2x2</a>
+	<a href="?nblignes=5&nbColonnes=5">5x5</a>
+	<a href="?nblignes=10&nbColonnes=8">10x8</a>
+	<?php
+	echo "<table border='1'>
+	<tbody>";
+	for($l=0;$l<=$_GET["nblignes"];$l++){
 
-        if (($l % 2) == 0){
-            $gras = "bold";
-        }
-        echo "<tr>"; 
-        for($c=0;$c<=$_GET["nbColonnes"];$c++){ 
-            echo "<td align='center'><p style='font-weight: bold;'>$l-$c</p></td>"; 
-        } 
-        echo "</tr>"; 
-    } 
-    echo "</tbody>
-    </table>";
-    ?>
+		if (($l % 2) == 0){
+			$gras = "bold";
+		}
+		else{
+			$gras = "normal";
+		}
+		echo "<tr>"; 
+		for($c=0;$c<=$_GET["nbColonnes"];$c++){ 
+			if (($c % 2) == 1){
+				$color = "red";
+			}
+			else{
+				$color = "black";
+			}
+			echo "<td align='center'><p style='font-weight: $gras; color: $color'>$l-$c</p></td>"; 
+		} 
+		echo "</tr>"; 
+	} 
+
+	echo "</tbody>
+	</table>";
+	?>
 </body>
 </html>
