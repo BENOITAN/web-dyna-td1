0adba1014ccabbb5b5bed56e3c3de8a3321069b8
BENOITAN <62307212+BENOITAN@users.noreply.github.com>
2020-04-01 09:06:16 +0200

    ez

---
+++ td1/exercice5/functions.php
@@ -0,0 +1,67 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>fonctions</title>
+	</head>
+	<body>
+	<?php
+		function createHtmlTables($nbLignes,$nbColonnes)
+	{?>
+	<table border='1'>
+			<tbody>
+				<?php 
+				for($i=0;$i<$nbLignes;$i++)
+				{?>
+					<tr>
+					<?php for($j=0;$j<$nbColonnes;$j++)
+					{?>
+						<td><?="$i - $j"?></td>
+					<?php }?>
+					</tr>
+				<?php }?>
+	<?php } ?>
+	
+	
+	
+	<?php
+		function element($titre,$contenu,$niveau)
+	{?>
+	<h<?php echo $niveau?>> <?php echo $titre ?> </h<?php echo $niveau?>>
+		
+	<?php } ?>
+	
+	
+	
+	<?php
+		function parseElements($element)
+	{
+	$elements=[
+		['titre'=>'Exercices',
+	'contenu'=>'liste des exercices disponibles'],	
+		['titre'=> 'exercice 1' ,
+	'contenu'=> 'fsv'],
+		['titre'=>'exercice 2',
+	'contenu'=>'Afficher le contenu d\'un tableau']
+	];
+	print_r($elements);
+	} ?>
+	
+	<?php
+		function wordCount($s) {
+			$nbmots=str_word_count($s);
+			echo 'Il y a '. $nbmots. ' mots dans la phrase';
+		}
+	?>
+	
+	<?php
+		function charCount($s, $c=NULL) {
+			$nbcaractère=strlen($s);
+			echo 'il y a '.$nbcaractère.' caractères dans la phrase';
+		}
+	?>
+	
+	
+	
+	
+	</body>
+</html>
